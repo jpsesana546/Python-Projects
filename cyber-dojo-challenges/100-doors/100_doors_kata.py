b0013949384aa3b6d27dbe0c05c909ff222ccdc3
João Sesana <joao.sesana@metro.ca>
2023-11-16 11:31:33 -0500

    First lines of code

--- cyber-dojo-challenges/100-doors/100_doors_kata.py
+++ cyber-dojo-challenges/100-doors/100_doors_kata.py
@@ -1,2 +1,11 @@
-if __name__ == '__main__':
-    pass
+class 100doors:
+
+    def create_100_doors(self) -> None:
+        pass
+
+#Create 100 doors
+    #Pass through each door
+    #Go back to beginning
+    #Pass through every other door
+    #Go back to beginning
+    #repeat
---
+++ cyber-dojo-challenges/100-doors/test_100_doors_kata.py
@@ -0,0 +1,4 @@
+import 100_doors_kata
+
+def test_should_create_100_doors(door_list):
+    assert len(door_list) == 100.
