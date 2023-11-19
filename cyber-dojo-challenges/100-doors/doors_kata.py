4ddefe540dcc845094415ae378fc2e404dbefa4a
João Sesana <joao.sesana@metro.ca>
2023-11-19 11:18:58 -0500

    First test

---
+++ cyber-dojo-challenges/100-doors/doors_kata.py
@@ -0,0 +1,17 @@
+class doors100():
+
+    def __init__(self) -> None:
+        door_list = []
+        pass
+
+    def create_100_doors(self,door_list) -> None:
+        for _ in range(100):
+            door_list.append(_)
+        return door_list
+        
+    #Create 100 doors
+        #Pass through each door
+        #Go back to beginning
+        #Pass through every other door
+        #Go back to beginning
+        #repeat
--- cyber-dojo-challenges/100-doors/test_100_doors_kata.py
+++ cyber-dojo-challenges/100-doors/test_100_doors_kata.py
@@ -1,4 +1,7 @@
-import 100_doors_kata
+from doors_kata import doors100
+door_list =[]
 
-def test_should_create_100_doors(door_list):
-    assert len(door_list) == 100.
+def test_should_create_100_doors():
+    doors = doors100()
+    doors.create_100_doors(door_list)
+    assert len(door_list) == 100
