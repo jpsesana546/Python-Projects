cd7ab2a9339555ddbf459b42f25760fd3e8bc618
João Sesana <joao.sesana@metro.ca>
2023-11-21 11:38:21 -0500

    added second function, not done

--- cyber-dojo-challenges/100-doors/doors_kata.py
+++ cyber-dojo-challenges/100-doors/doors_kata.py
@@ -4,14 +4,18 @@ class doors100():
         door_list = []
         pass
 
-    def create_100_doors(self,door_list) -> None:
+    def create_100_doors(self, door_list) -> None:
         for _ in range(100):
             door_list.append(_)
         return door_list
-        
-    #Create 100 doors
-        #Pass through each door
-        #Go back to beginning
-        #Pass through every other door
-        #Go back to beginning
-        #repeat
+
+    def pass_by_door(self, door_list):
+        for door in door_list:
+            print(f"Door {door}")
+
+    # Create 100 doors
+        # Pass through each door
+        # Go back to beginning
+        # Pass through every other door
+        # Go back to beginning
+        # repeat
